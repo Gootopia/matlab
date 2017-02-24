cece4a90394d0a99c8167df76cfea8b08abc58d6
MRK <mattkerschbaum@hotmail.com>
2017-02-24 06:26:36 -0800

    Added SMA data lines to default plot.

--- charting/stockChart.m
+++ charting/stockChart.m
@@ -2,6 +2,9 @@ function stockChart(ti)
 %stockChart(ti)
 %  ti:  trade instrument
 
+SMAS = 43;
+SMAL = 252;
+
 % Create figure
 figure1 = figure;
 
@@ -9,11 +12,21 @@ figure1 = figure;
 axes1 = axes('Parent',figure1,'Position',[0.03 0.05 0.95 0.9]);
 hold(axes1,'on');
 
-% Create plot
-plot1 = plot(ti.close);
-plot1.MarkerSize = 2;
-plot1.MarkerEdgeColor = 'r';
-plot1.Marker = 'o';
+% Create plot of closing price data
+plot_price = plot(ti.close);
+plot_price.MarkerSize = 2;
+plot_price.MarkerEdgeColor = 'r';
+plot_price.Marker = 'o';
+plot_price.LineWidth = 1.5;
+
+% Create SMA
+data_smas = sma(ti.close, SMAS);
+data_smal = sma(ti.close, SMAL);
+
+plot_smas = plot(data_smas);
+plot_smas.Color = [0 0.8 0];
+plot_smal = plot(data_smal);
+plot_smal.Color = [0.8 0 0];
 
 % lines on top/right edges
 box(axes1,'on');
