eb1d2630966cff0188af8824a6edf7dd174c6beb
MRK <mattkerschbaum@hotmail.com>
2017-03-02 05:56:05 -0800

    Chart now only shows a portion of the data when created.

--- charting/stockChart.m
+++ charting/stockChart.m
@@ -1,38 +1,71 @@
-function stockChart(ti,type)
+function stockChart(ti,type,showclose)
 %stockChart(ti)
 % ti:  trade instrument
 % type: 'line' (default), 'ha' (HeikenAshi)
 SMAS = 43;
 SMAL = 252;
+% number of price bars to show
+SHOWBARS = 250;
 
-% Default close is just our regular price
+% Default close is just our regular price.
 close = ti.close;
+% number of price bars in the data
+nbars = length(close);
 
-% Default security type is 'STK'
+% Default drawing price is our line
 if ~exist('type', 'var') || isempty(type)
     type = 'line';
 end
-            
-% Create figure
+
+% Default is not to show a line plot with the closing prices.
+if ~exist('showclose', 'var') || isempty(type)
+    showclose = 'no';
+end
+   
+%===== SET UP THE GRAPHICS =====
 figure1 = figure;
 
 % Create axes. Expand to fill more of screen
 axes1 = axes('Parent',figure1,'Position',[0.03 0.05 0.95 0.9]);
 % lines on top/right edges
 box(axes1,'on');
+
+% Add grid lines
+grid on
+grid minor
+
+% If data is too large to reasonably fit on the screen, set the xlimits to
+% only show the most recent data. +1 so last point isn't truncated.
+if nbars > SHOWBARS
+    x_max = nbars+1;
+    x_min = nbars-SHOWBARS;
+    xlim([x_min x_max]);
+end
+
+% 
 hold(axes1,'on');
 
+%===== CREATE THE CHART =====
 % Create items needed for HeikenAshi
-if strcmp(type,'ha') == 1
-    [ho,hh,hl,hc] = heikenAshi(ti);
-    candlestick(ho', hh', hl', hc');
-else
+switch(type)
+    % Standard line drawing
+    case 'line'
+        plot(close);
+        
+    % Heiken Ashi candlesticks
+    case 'ha'
+        [ho,hh,hl,hc] = heikenAshi(ti);
+        candlestick(ho', hh', hl', hc');
+end
+
+% Show a plot with the actual closing prices.
+if strcmp(showclose, 'showclose') 
     % Create standard line plot
-    plot_price = plot(ti.close);
-    plot_price.MarkerSize = 2;
-    plot_price.MarkerEdgeColor = 'r';
-    plot_price.Marker = 'o';
-    plot_price.LineWidth = 1.5;
+    markers_close = plot(ti.close, 'o');
+    markers_close.MarkerSize = 2;
+    markers_close.MarkerEdgeColor = [0 0 0.8];
+    %plot_price.MarkerEdgeColor = 'r';
+    %plot_price.LineWidth = 1.5;
 end
 
 %===== ADD INDICATORS TO DRAWINGS =====
@@ -44,7 +77,4 @@ plot_smas.Color = [0 0.8 0];
 plot_smal = plot(data_smal);
 plot_smal.Color = [0.8 0 0];
 
-% Put 
-% Add grid lines
-grid on
-grid minor
+hold off
